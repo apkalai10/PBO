eebeb184148e5227801cc4ad12af2f48ed1b0e68
aravinthprasath.e <aravinthprasath.e@ABHWSST010.abhayaa.com>
2024-04-02 18:07:56 +0530

    TestNG - Test annotations added

--- com.al/src/main/java/com/java/HashMaps.java
+++ com.al/src/main/java/com/java/HashMaps.java
@@ -2,17 +2,23 @@ package com.java;
 
 import java.util.HashMap;
 
+import org.testng.annotations.Test;
+
 public class HashMaps {
 
-	public static void main(String[] args) {
+	
+	@Test
+	public void data()
+	{
 		
 		HashMap<String, String> data = new HashMap<String, String>();
 		data.put("TN", "Tamilnadu");
 		data.put("KL", "Kerala");
 		data.put("AP", "Andhra Pradesh");
 		System.out.println(data);
+		System.out.println(data.size());
 		
-
+		
 	}
 
 }
