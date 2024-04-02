a2b23db46db15fff339b1001b02fb625f727e994
aravinthprasath.e <aravinthprasath.e@ABHWSST010.abhayaa.com>
2024-04-02 17:58:22 +0530

    classpath and hashMaps added

---
+++ com.al/src/main/java/com/java/HashMaps.java
@@ -0,0 +1,18 @@
+package com.java;
+
+import java.util.HashMap;
+
+public class HashMaps {
+
+	public static void main(String[] args) {
+		
+		HashMap<String, String> data = new HashMap<String, String>();
+		data.put("TN", "Tamilnadu");
+		data.put("KL", "Kerala");
+		data.put("AP", "Andhra Pradesh");
+		System.out.println(data);
+		
+
+	}
+
+}
